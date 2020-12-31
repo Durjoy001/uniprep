38b9b91067b5133596ce33fe74b688ec34cd2651
Imrul Kayes Sifat <41978816+ImrulkayeSifat@users.noreply.github.com>
2020-12-31 08:25:11 -0800

    Add files via upload

---
+++ uniprep/course/admissionTest/physic/app.js
@@ -0,0 +1,40 @@
+const quizForm = document.querySelector('.qns-form');
+const resultPanel = document.querySelector('#result');
+const Marks = document.querySelector('span.marks');
+const testAnswers = ['A', 'B', 'B', 'C', 'B'];
+
+quizForm.addEventListener('submit', e => {
+  e.preventDefault();
+
+  let userAnswers = [quizForm.qn1.value, quizForm.qn2.value, quizForm.qn3.value, quizForm.qn4.value, quizForm.qn5.value];
+  let score = 0;
+
+  userAnswers.forEach((answer, index) => {
+    if (answer === testAnswers[index]) {
+      score += 20;
+    }
+  });
+
+  resultPanel.classList.remove('hide');
+
+
+  scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+
+  let output = 0;
+
+  const counter = setInterval(() => {
+    Marks.textContent = `${output}%`;
+
+    if (output === score) {
+      clearInterval(counter);
+    } else {
+      output++;
+    }
+  }, 10)
+
+  quizForm.reset();
+
+});
