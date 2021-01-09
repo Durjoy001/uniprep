52d69489f7a4250387c7e60c82f9b2708a51b2ec
ImrulkayeSifat <kayesimrul224@gmail.com>
2021-01-08 22:26:56 -0800

    ajax word countdown timer

---
+++ authentication/ajax/load_total_que.php
@@ -0,0 +1,10 @@
+<?php
+
+    session_start();
+    include "../connection.php";
+    $total_que=0;
+    $res1=mysqli_query($db,"select * from questions where category='$_SESSION[exam_category]'");
+    $total_que=mysqli_num_rows($res1);
+    echo $total_que;
+
+?>
