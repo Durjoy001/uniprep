3bed7be687f1921977cf35a055a435aaf3ccfb0c
ImrulkayeSifat <kayesimrul224@gmail.com>
2021-01-08 22:38:29 -0800

    fixed some bug

--- admin/header.php
+++ admin/header.php
@@ -6,11 +6,8 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title> Admin Panel</title>
-    <meta name="description" content="Sufee Admin - HTML5 Admin Template">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <link rel="apple-touch-icon" href="apple-icon.png">
-    <link rel="shortcut icon" href="favicon.ico">
 
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
--- authentication/login.php
+++ authentication/login.php
@@ -1,5 +1,6 @@
 <?php
- include "connection.php";
+session_start();
+include "connection.php";
 ?>
 
 <!doctype html>
@@ -12,22 +13,22 @@
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-    
+
 </head>
 
 <body>
 
-	<div class="error-pagewrap">
-		<div class="error-page-int">
-			<div class="text-center custom-login">
-				<h3>Login Now</h3>
+    <div class="error-pagewrap">
+        <div class="error-page-int">
+            <div class="text-center custom-login">
+                <h3>Login Now</h3>
 
-			</div>
-			<div class="content-error">
-				<div class="hpanel">
+            </div>
+            <div class="content-error">
+                <div class="hpanel">
                     <div class="panel-body">
                         <form action="" name="form1" method="post">
                             <div class="row">
@@ -39,7 +40,7 @@
                                     <label>Password</label>
                                     <input type="password" name="password" class="form-control">
                                 </div>
-                              </div>
+                            </div>
                             <div class="text-center">
                                 <button type="submit" name="login" class="btn btn-success loginbtn">Login</button>
                             </div>
@@ -48,43 +49,40 @@
                             </div>
 
                             <div class="alert alert-danger" id="failure" style="display:none">
-                                <strong>Error!</strong> Wrong username  & password!
+                                <strong>Error!</strong> Wrong username & password!
                             </div>
 
-                           
+
                         </form>
                     </div>
                 </div>
-			</div>
+            </div>
 
-		</div>   
+        </div>
     </div>
 
     <?php
-        if(isset($_POST["login"]))
-        {
-            $count=0;
-            $res=mysqli_query($db,"select * from registration where username='$_POST[username]' && password='$_POST[password]'");
-            $count=mysqli_num_rows($res);
+    if (isset($_POST["login"])) {
+        $count = 0;
+        $res = mysqli_query($db, "select * from registration where username='$_POST[username]' && password='$_POST[password]'");
+        $count = mysqli_num_rows($res);
 
-            if($count==0)
-            {
-                ?>
-                    <script type="text/javascript">
-                        
-                        document.getElementById("failure").style.display="block";
-                    </script>
-                <?php
-            }
-            else
-            {
-                ?>
-                    <script type="text/javascript">
-                        window.location="demo.php";
-                    </script>
-                <?php
-            }
+        if ($count == 0) {
+
+    ?>
+            <script type="text/javascript">
+                document.getElementById("failure").style.display = "block";
+            </script>
+        <?php
+        } else {
+            $_SESSION["username"] = $_POST["username"];
+        ?>
+            <script type="text/javascript">
+                window.location = "select_exam.php";
+            </script>
+    <?php
         }
+    }
     ?>
 </body>
 
